d6bfa0344e5ed4872a35d63a836bdcae96f619bb
Staszek <stanislaw.polanski@hotmail.com>
2018-07-05 21:51:32 +0200

    RoundRobinScheduler stub created

---
+++ FootballSchedulerDLL/RoundRobinScheduler.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using FootballSchedulerWPF;
+
+namespace FootballSchedulerDLL
+{
+    class RoundRobinScheduler : IScheduler
+    {
+        public void GenerateSchedule()
+        {
+            throw new NotImplementedException();
+        }
+
+        public List<Matches> GetSchedule()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void LoadLeague(Leagues league)
+        {
+            throw new NotImplementedException();
+        }
+
+        public bool LoadTeams(List<Teams> teams)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
