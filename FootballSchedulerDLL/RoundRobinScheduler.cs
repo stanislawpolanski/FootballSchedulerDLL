5dd69372be7bfeab8e7b887998f1aba7952b2ca5
Staszek <stanislaw.polanski@hotmail.com>
2018-07-18 22:15:12 +0200

    all pre-designed test passed

--- FootballSchedulerDLL/RoundRobinScheduler.cs
+++ FootballSchedulerDLL/RoundRobinScheduler.cs
@@ -32,16 +32,17 @@ namespace FootballSchedulerDLL
             //prepare datetime - matches played on sundays
             DateTime firstRoundStartDate = new DateTime(YearOfStart.Year, 2, 1);
             while (firstRoundStartDate.DayOfWeek != DayOfWeek.Sunday)
-                firstRoundStartDate.AddDays(1);
+                firstRoundStartDate = firstRoundStartDate.AddDays(1);
 
             DateTime secondRoundStartDate = new DateTime(YearOfStart.Year, 8, 1);
             while (secondRoundStartDate.DayOfWeek != DayOfWeek.Sunday)
-                secondRoundStartDate.AddDays(1);
+                secondRoundStartDate = secondRoundStartDate.AddDays(1);
 
             //generate the schedule
             this.Schedule = new List<Matches>();
-            for (int round = 1; round < teamsQueue.Count; round++)
+            for (int round = 0; round < teamsQueue.Count; round++)
             {
+                //recreate team's list
                 LinkedList<Teams> teamsLinkedList = new LinkedList<Teams>(teamsQueue);
                 teamsLinkedList.AddFirst(fixedTeam);
 
@@ -53,26 +54,35 @@ namespace FootballSchedulerDLL
                     Matches m1 = new Matches();
                     m1.HomeTeamId = t1.Id;
                     m1.AwayTeamId = t2.Id;
-                    m1.TimeOfPlay = firstRoundStartDate.AddDays(7 * (round - 1));
-
+                    
                     Matches m2 = new Matches();
                     m2.HomeTeamId = t2.Id;
                     m2.AwayTeamId = t1.Id;
-                    m2.TimeOfPlay = secondRoundStartDate.AddDays(7 * (round - 1));
-
-
-                    teamsLinkedList.RemoveFirst();
-                    teamsLinkedList.RemoveLast();
-
+                    
                     //every even round switch every home & away team
                     if(round % 2 != 0)
                     {
                         Matches mTemp = m1;
                         m1 = m2;
-                        m1 = mTemp;
+                        m2 = mTemp;
                     }
+
+                    //set dates
+                    m1.TimeOfPlay = firstRoundStartDate.AddDays(7 * round);
+                    m2.TimeOfPlay = secondRoundStartDate.AddDays(7 * round);
+
+                    //add matches to the schedule
+                    this.Schedule.Add(m1);
+                    this.Schedule.Add(m2);
+
+                    //and then remove both teams from the current round
+                    teamsLinkedList.RemoveFirst();
+                    teamsLinkedList.RemoveLast();
                 }
                 while (teamsLinkedList.Count > 1);
+
+                //take last team into the beginning of the queue
+                teamsQueue.Enqueue(teamsQueue.Dequeue());
             }
         }
 
@@ -90,11 +100,25 @@ namespace FootballSchedulerDLL
 
         public bool LoadTeams(List<Teams> teams)
         {
+            //check if teams are not null
+            if (teams == null)
+                return false;
             //check if there are at least two teams and the team's number is even
-            if (teams.Count < 2 || teams.Count % 2 != 0 || teams == null)
+            if (teams.Count < 2 || teams.Count % 2 != 0)
                 return false;
 
-            //if the check above is ok then assign the property and return true
+            //check if each team have distinctive id
+            //prepare a list for comparison
+            List<int> checkList = new List<int>();
+
+            foreach(Teams t in teams)
+            {
+                if (checkList.Exists(id => id == t.Id))
+                    return false;
+                checkList.Add(t.Id);
+            }
+
+            //if the all the checks above are ok then assign the property and return true
             this.LoadedTeams = teams;
             return true;
         }
--- FootballSchedulerDLLTests/RoundRobinTests.cs
+++ FootballSchedulerDLLTests/RoundRobinTests.cs
@@ -18,6 +18,7 @@ namespace FootballSchedulerDLLTests
             lt.Add(new Teams());
 
             RoundRobinScheduler rrs = new RoundRobinScheduler();
+
             //act
             bool loaded = rrs.LoadTeams(lt);
             //assert
@@ -27,28 +28,65 @@ namespace FootballSchedulerDLLTests
         [TestMethod]
         public void LoadTeams_WhenTeamsCountNotEven_ReturnsFalse()
         {
-            throw new AssertInconclusiveException();
             //arrange
+            List<Teams> teams = new List<Teams>();
+            teams.Add(new Teams());
+            teams.Add(new Teams());
+            teams.Add(new Teams());
+
+            RoundRobinScheduler rrs = new RoundRobinScheduler();
+
             //act
+            bool loaded = rrs.LoadTeams(teams);
+
             //assert
+            Assert.IsFalse(loaded);
         }
 
         [TestMethod]
         public void LoadTeams_WhenTeamsNull_ReturnsFalse()
         {
-            throw new AssertInconclusiveException();
             //arrange
+            List<Teams> teams = null;
+
+            RoundRobinScheduler rrs = new RoundRobinScheduler();
+
             //act
+            bool loaded = rrs.LoadTeams(teams);
+
             //assert
+            Assert.IsFalse(loaded);
         }
 
         [TestMethod]
-        public void LoadTeams_AllTeamsMustHaveDistinctiveId()
+        public void LoadTeams_WhenTeamsHaveNoDistinctiveIds_ReturnsFalse()
         {
-            throw new AssertInconclusiveException();
             //arrange
+            List<Teams> teams = new List<Teams>();
+
+            for(int i = 0; i < 17; i++)
+            {
+                Teams t = new Teams
+                {
+                    Id = i
+                };
+
+                teams.Add(t);
+            }
+
+            Teams nonDistinctiveIdTeam = new Teams
+            {
+                Id = 15
+            };
+            teams.Add(nonDistinctiveIdTeam);
+
+            RoundRobinScheduler rrs = new RoundRobinScheduler();
+
             //act
+            bool loaded = rrs.LoadTeams(teams);
+
             //assert
+            Assert.IsFalse(loaded);
         }
 
         [TestMethod]
@@ -65,10 +103,68 @@ namespace FootballSchedulerDLLTests
         [TestMethod]
         public void GenerateSchedule_ScheduleMustCoverAllPairs()
         {
-            throw new AssertInconclusiveException();
-            //arrange
-            //act
-            //assert
+            //ARRANGE
+            int teamsNumber = 8;
+
+            //prepare checklist
+            bool[][] checkList = new bool[teamsNumber][];
+            for(int i = 0; i < teamsNumber; i++)
+            {
+                checkList[i] = new bool[teamsNumber];
+            }
+
+            //create teams
+            List<Teams> teams = new List<Teams>();
+
+            for(int i = 1; i < teamsNumber + 1; i++)
+            {
+                Teams t = new Teams
+                {
+                    Id = i
+                };
+
+                teams.Add(t);
+            }
+
+            //invoke scheduler
+            RoundRobinScheduler rrs = new RoundRobinScheduler();
+
+            Leagues league = new Leagues();
+            rrs.LoadLeague(league);
+
+            rrs.YearOfStart = new DateTime(2017, 3, 8);
+
+            rrs.LoadTeams(teams);
+
+            //ACT
+            rrs.GenerateSchedule();
+            List<Matches> matches = rrs.GetSchedule();
+
+            //ASSERT
+            matches.ForEach(x => checkList[(int)x.HomeTeamId - 1][(int)x.AwayTeamId - 1] = true);
+
+
+
+            //check if all of the matches has been played
+            //however any team can not play itself
+            for(int i = 0; i < teamsNumber; i++)
+            {
+                for(int j = 0; j < teamsNumber; j++)
+                {
+                    bool iEqualsJ    = (i == j);
+                    bool matchPlayed = checkList[i][j];
+
+                    //team cannot play itself!
+                    if (iEqualsJ && matchPlayed)
+                        Assert.Fail();
+
+                    //all the matches must be played, if not, then fail
+                    if (!iEqualsJ && !matchPlayed)
+                        Assert.Fail();
+                }
+            }
+
+            //if not failed, then ok
         }
     }
 }
