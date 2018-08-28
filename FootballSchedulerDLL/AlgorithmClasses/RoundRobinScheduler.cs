43a314b3b4f3559ec7b57d3af887127d6980632f
Staszek <stanislaw.polanski@hotmail.com>
2018-08-28 19:15:49 +0200

    RoundRobinScheduler.GenerateSchedule refactored (split into smaller methods)

--- FootballSchedulerDLL/AlgorithmClasses/RoundRobinScheduler.cs
+++ FootballSchedulerDLL/AlgorithmClasses/RoundRobinScheduler.cs
@@ -9,6 +9,10 @@ namespace FootballSchedulerDLL
         private List<Match> Schedule;
         private List<Team> LoadedTeams;
         private League LoadedLeague;
+        /// <summary>
+        /// Contains dates of dates when each of the rounds starts (usually spring & autumn).
+        /// </summary>
+        private Tuple<DateTime, DateTime> DatesOfRoundsStarts;
 
         /// <summary>
         /// Year of league's start. Only year is considered.
@@ -16,23 +20,19 @@ namespace FootballSchedulerDLL
         public DateTime YearOfStart { get; set; }
 
         /// <summary>
-        /// Calculates the schedule using inputs set before.
+        /// Checks if teams and league are loaded.
         /// </summary>
-        /// <exception cref="System.InvalidOperationException">Thrown when no teams where loaded or no league loaded.</exception>
-        public void GenerateSchedule()
+        private void InputsCheck()
         {
-            //inputs check
             if (this.LoadedTeams == null)
                 throw new InvalidOperationException("No teams loaded");
 
             if (this.LoadedLeague == null)
                 throw new InvalidOperationException("No league assigned");
+        }
 
-            //prepare queue list and get the fixed team
-            Queue<Team> teamsQueue = new Queue<Team>(this.LoadedTeams);
-            Team fixedTeam = teamsQueue.Dequeue();
-
-            //prepare datetime - matches played on sundays
+        private Tuple<DateTime, DateTime> CalculateDatesOfRoundsBeginnings()
+        {
             DateTime firstRoundStartDate = new DateTime(YearOfStart.Year, 8, 1, 15, 0, 0);
             while (firstRoundStartDate.DayOfWeek != DayOfWeek.Sunday)
                 firstRoundStartDate = firstRoundStartDate.AddDays(1);
@@ -41,8 +41,25 @@ namespace FootballSchedulerDLL
             while (secondRoundStartDate.DayOfWeek != DayOfWeek.Sunday)
                 secondRoundStartDate = secondRoundStartDate.AddDays(1);
 
-            //prepare the schedule
+            return new Tuple<DateTime, DateTime>(firstRoundStartDate, secondRoundStartDate);
+        }
+
+        /// <summary>
+        /// Calculates the schedule using inputs set before.
+        /// </summary>
+        /// <exception cref="System.InvalidOperationException">Thrown when no teams where loaded or no league loaded.</exception>
+        public void GenerateSchedule()
+        {
+            //inputs check
+            this.InputsCheck();
+
+            //prepare queue list and get the fixed team, prepare the schedule
+            Queue<Team> teamsQueue = new Queue<Team>(this.LoadedTeams);
+            Team fixedTeam = teamsQueue.Dequeue();
             this.Schedule = new List<Match>();
+
+            //prepare datetime - matches played on sundays
+            DatesOfRoundsStarts = this.CalculateDatesOfRoundsBeginnings();
 
             //generate the schedule
             for (int round = 0; round < teamsQueue.Count; round++)
@@ -54,35 +71,16 @@ namespace FootballSchedulerDLL
 
                 do
                 {
+                    
+                    //TODO Refactor - kick out inner loop into function
                     Team t1 = teamsLinkedList.First.Value;
                     Team t2 = teamsLinkedList.Last.Value;
 
-                    //each team must play both home and away
-                    Match m1 = new Match();
-                    m1.HomeTeamId = t1.Id;
-                    m1.AwayTeamId = t2.Id;
-                    m1.LeagueId   = this.LoadedLeague.Id;
-                    
-                    Match m2 = new Match();
-                    m2.HomeTeamId = t2.Id;
-                    m2.AwayTeamId = t1.Id;
-                    m2.LeagueId   = this.LoadedLeague.Id;
-                    
-                    //every even round switch every home & away team
-                    if(round % 2 != 0)
-                    {
-                        Match mTemp = m1;
-                        m1 = m2;
-                        m2 = mTemp;
-                    }
-
-                    //set dates
-                    m1.TimeOfPlay = firstRoundStartDate.AddDays(7 * round);
-                    m2.TimeOfPlay = secondRoundStartDate.AddDays(7 * round);
+                    Tuple<Match, Match> pairOfMatches = this.GeneratePairOfMatches(round, t1, t2);
 
                     //add matches to the schedule
-                    this.Schedule.Add(m1);
-                    this.Schedule.Add(m2);
+                    this.Schedule.Add(pairOfMatches.Item1);
+                    this.Schedule.Add(pairOfMatches.Item2);
 
                     //and then remove both teams from the current round
                     teamsLinkedList.RemoveFirst();
@@ -93,6 +91,41 @@ namespace FootballSchedulerDLL
                 //take last team into the beginning of the queue
                 teamsQueue.Enqueue(teamsQueue.Dequeue());
             }
+
+            this.DatesOfRoundsStarts = null;
+        }
+
+        private Tuple<Match, Match> GeneratePairOfMatches(int round, Team team1, Team team2)
+        {
+            //each team must play both home and away
+            Match m1 = new Match()
+            {
+                HomeTeamId = team1.Id,
+                AwayTeamId = team2.Id,
+                LeagueId = this.LoadedLeague.Id
+            };
+
+
+            Match m2 = new Match()
+            {
+                HomeTeamId = team2.Id,
+                AwayTeamId = team1.Id,
+                LeagueId = this.LoadedLeague.Id
+            };
+
+            //every even round switch every home & away team
+            if (round % 2 != 0)
+            {
+                Match mTemp = m1;
+                m1 = m2;
+                m2 = mTemp;
+            }
+
+            //set dates
+            m1.TimeOfPlay = this.DatesOfRoundsStarts.Item1.AddDays(7 * round);
+            m2.TimeOfPlay = this.DatesOfRoundsStarts.Item2.AddDays(7 * round);
+
+            return new Tuple<Match, Match>(m1, m2);
         }
 
         /// <summary>
